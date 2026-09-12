651126ae10f141e22bbeb092c4e011050c932585
fbrugs <81498392+fbrugs@users.noreply.github.com>
2026-09-12 04:47:10 +0200

    single item remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === remove) {
+            array.splice(i, 1);
+        }
+    }
+    return array
 };
+
+
+let x = ['hello', 'my', 'name', 'is', 'fabian']
+
+removeFromArray(x, 'test');
 
 // Do not edit below this line
 module.exports = removeFromArray;
