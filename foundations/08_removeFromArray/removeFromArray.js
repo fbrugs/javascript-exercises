21aa296f925f7434aa69f1e857cedb67b73a0908
fbrugs <81498392+fbrugs@users.noreply.github.com>
2026-09-12 06:35:55 +0200

    Finished js exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,17 +1,29 @@
-const removeFromArray = function(array, remove) {
-    
+const removeFromArray = function(array, ...remove) {
+
+    let newArray = [];
+
+    function duplicate(item, removeitems) {
+        let found = false;
+
+        for (const rem in removeitems) {
+            if (removeitems[rem] === item) {
+                found = true
+            }
+        }
+        return found
+    }
+
+
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === remove) {
-            array.splice(i, 1);
+        if (!duplicate(array[i], remove)) {
+            newArray.push(array[i])
         }
     }
-    return array
+    
+    return newArray
+
 };
 
-
-let x = ['hello', 'my', 'name', 'is', 'fabian']
-
-removeFromArray(x, 'test');
 
 // Do not edit below this line
 module.exports = removeFromArray;
