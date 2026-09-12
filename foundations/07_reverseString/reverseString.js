0ad74da096a44d937a576cee3216dff32eb64c90
fbrugs <81498392+fbrugs@users.noreply.github.com>
2026-09-12 04:29:37 +0200

    Finished js exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(inp) {
+     let stringArray = inp.split("");
+     let reverseString = stringArray.reverse();
+     let finalString = '';
 
+     for (let i = 0; i < reverseString.length; i++) {
+        finalString += reverseString[i];
+     }
+     
+     return finalString;
+     
 };
 
+
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
