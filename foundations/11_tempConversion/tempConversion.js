4a7cf3914cd13d3b7bff22346d25cae76d2a7ec4
fbrugs <81498392+fbrugs@users.noreply.github.com>
2026-09-13 04:03:15 +0200

    Finished js exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+
+  if (celsius % 1 !== 0) {
+    return +celsius.toFixed(1);
+  } else {
+  return celsius
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit % 1 !== 0 ? +fahrenheit.toFixed(1) : fahrenheit
 };
+
 
 // Do not edit below this line
 module.exports = {
