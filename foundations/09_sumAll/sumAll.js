a3614422b5ea8d099613e1d563598d1588b1f633
fbrugs <81498392+fbrugs@users.noreply.github.com>
2026-09-13 02:20:03 +0200

    Finished js exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(lowNum, highNum) {
+    let outcome = 0;
+    let firstNum =0;
+    let secNum = 0;
 
+    if (typeof lowNum === 'number' && typeof highNum === 'number') {
+        if (lowNum % 1 === 0 && highNum % 1 === 0) {
+            if (lowNum > 0 && highNum > 0) {
+                if (lowNum > highNum) {
+                    firstNum = highNum;
+                    secNum = lowNum;
+                } else  {
+                    firstNum = lowNum;
+                    secNum = highNum;
+                }
+
+                for (let i = firstNum; i <= secNum; i++) {
+                    outcome += i;
+                }
+
+                return outcome
+            } else {
+                return 'ERROR'
+            }
+        } else {
+            return 'ERROR'
+        }
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
